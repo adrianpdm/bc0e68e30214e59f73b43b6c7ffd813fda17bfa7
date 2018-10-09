7fecb398aa517153b24c913fbc21a3f31ed06a7b
Adrian <adrian.padmodihardjo@gmail.com>
2018-10-09 22:38:46 +0700

    remove slot name to simplify templating

--- component/HeadlineGrid.js
+++ component/HeadlineGrid.js
@@ -8,7 +8,9 @@ const HeadlineGrid = {
 			`<div class="row">`+
 				`<div class="col-md-3 headline-title"><span>{{headlineTitle}}</span></div>` +
 				`<div class="col-md-1"></div>`+
-				`<div class="col-md-8"><slot name='content'></slot></div>` +
+				`<div class="col-md-8">`+
+					`<slot></slot>`+
+				`</div>` +
 			`</div>`+
         `</div>`
         //@formatter:on
