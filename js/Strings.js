996c4e1335102f22c37f9e6558875bd996446fc1
Adrian <adrian.padmodihardjo@gmail.com>
2018-10-10 02:44:33 +0700

    separate strings for future localization

---
+++ js/Strings.js
@@ -0,0 +1,24 @@
+const JoinImpact = "Join an Impactful Company"
+const WhatWeDo = "What We Do"
+const ExploreOpp = "Explore Opportunities"
+
+const WhatWeDo_desc =
+	`Kulina's mission is to "serve the best meal with an outstanding customer experience at the lowest possible price every single day". ` +
+	`We want to be customer's default meal option by providing quality product with on-time delivery. ` +
+	`We help customers save time and money for each meal they get from us, so they can better spend it on other things that matter. ` +
+	`<br><br>` +
+	`We took pride in being the first food tech startup to join "Google Launchpad Accelerator" in San Francisco, and the only on from Indonesia in winter 2018. ` +
+	`Our startup is backed by the same investor who invested in Ele.me (China's unicorn food delivery startup). ` +
+	`<br><br>` +
+	`Join us in our mission to delivering the best meals to ten millions of Jakartans. ` +
+	`Hurry! Be part of our Kulinas family.`
+
+const Apply = "Apply"
+
+const Strings = {
+	JoinImpact,
+	WhatWeDo,
+	WhatWeDo_desc,
+	ExploreOpp,
+	Apply
+}
