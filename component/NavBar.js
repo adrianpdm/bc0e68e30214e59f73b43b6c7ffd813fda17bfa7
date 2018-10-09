3b553f52ec13703c340b01e16a5835ded52ed4d1
Adrian <adrian.padmodihardjo@gmail.com>
2018-10-10 01:49:49 +0700

    fix navbar styling

--- component/NavBar.js
+++ component/NavBar.js
@@ -12,19 +12,22 @@ const NavigationBar = {
 		}
 	},
 	template:
-		`<nav class="navbar navbar-light navbar-expand-md fixed-top">`+
-            `<a class="navbar-brand kulina-brand" href="#"><img :src="brandIcon"></a>`+
-            `<button class="navbar-toggler" type="button" data-toggle="collapse" href="#navbar-header" aria-controls="navbar-header" aria-expanded="false" aria-label="Open Drawer">`+
-                `<span class="navbar-toggler-icon"></span>`+
-            `</button>`+
-			`<div class="collapse navbar-collapse" id="navbar-header">`+
-				`<ul class="navbar-nav">`+
-					`<template v-for="(item, i) in menu">`+
-						`<li class="nav-item" :class="{'active': item.isActive}" :key="'item-'+i">`+
-							`<a class="nav-link" href="#">{{item.name}}</a>`+
-						`</li>`+
-					`</template>`+
-				`</ul>`+
-			`</div>`+
-        `</nav>`
+		`<div class="navbar-container">`+
+			`<div class="navbar-background"></div>`+
+			`<nav class="navbar navbar-light navbar-expand-md fixed-top">`+
+				`<a class="navbar-brand kulina-brand" href="#"><img :src="brandIcon"></a>`+
+				`<button class="navbar-toggler" type="button" data-toggle="collapse" href="#navbar-header" aria-controls="navbar-header" aria-expanded="false" aria-label="Open Drawer">`+
+					`<span class="navbar-toggler-icon"></span>`+
+				`</button>`+
+				`<div class="collapse navbar-collapse" id="navbar-header">`+
+					`<ul class="navbar-nav">`+
+						`<template v-for="(item, i) in menu">`+
+							`<li class="nav-item" :class="{'active': item.isActive}" :key="'item-'+i">`+
+								`<a class="nav-link" href="#">{{item.name}}</a>`+
+							`</li>`+
+						`</template>`+
+					`</ul>`+
+				`</div>`+
+			`</nav>`+
+		`</div>`
 }
